f1280ab16d458b436376a16917ed1db1513a129e
a.dernovskiy <dernovski@icloud.com>
2025-11-30 15:22:24 +0700

    fix for tests

--- server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -6,10 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 public record UserDto(
 
         Long id,
-        @NotBlank(message = "Name must not be blank")
         String name,
-        @NotBlank(message = "Email must not be blank")
-        @Email(message = "Email must be correct")
         String email
 ) {
 }
